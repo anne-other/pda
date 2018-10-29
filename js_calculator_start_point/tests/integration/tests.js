d7655bf3f31cc96ff2059c9f906980d5f44563e2
anne-other <anne.c.ince@gmail.com>
2018-10-29 13:54:56 +0000

    calculator calculation not affected by clear in integration test

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -43,4 +43,14 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('7')
   })
 
+  it('should be able to continue the calculation if clear is clicked', function () {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number8')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#clear')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('2')
+  })
+
 });
