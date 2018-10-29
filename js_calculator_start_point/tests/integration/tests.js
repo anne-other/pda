e078b30dacdddea85551a1e26ad2e8c7cbc4acf9
anne-other <anne.c.ince@gmail.com>
2018-10-29 14:51:04 +0000

    calculator displays not a number when something is devided by zero

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -106,4 +106,13 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('1e+26')
   })
 
+  it('should display not a number if a devision by zero is made', function () {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number5')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('NaN')
+  })
+
 });
