b81acb4a7323bbe06794ab0651d4a9900ac42b60
anne-other <anne.c.ince@gmail.com>
2018-10-29 13:40:31 +0000

    calculator operational operators update display in integration test

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -23,4 +23,13 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('19')
   })
 
+  it('should adjust the desplay to take into account operators used', function () {
+  running_total = element(by.css('#running_total'))
+  element(by.css('#number3')).click();
+  element(by.css('#operator_add')).click();
+  element(by.css('#number4')).click();
+  element(by.css('#operator_equals')).click();
+  expect(running_total.getAttribute('value')).to.eventually.equal('7')
+  })
+
 });
