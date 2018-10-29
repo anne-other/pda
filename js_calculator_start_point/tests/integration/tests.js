e46721e6a2ad21c5c6ab426cd4b5b780353ebeaf
anne-other <anne.c.ince@gmail.com>
2018-10-29 13:47:29 +0000

    calculator chain operators in integration test

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -32,4 +32,15 @@ describe('calculator functionality', function() {
   expect(running_total.getAttribute('value')).to.eventually.equal('7')
   })
 
+  it('should be able to chain multiple operators', function () {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number6')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number7')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('7')
+  })
+
 });
